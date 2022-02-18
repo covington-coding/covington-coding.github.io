039e14ba9732cd10533c8925ec343f25214936ce
covington-coding <covington.coding@gmail.com>
2022-02-18 17:42:16 +0000

    about me

---
+++ css_helpers.js
@@ -0,0 +1,27 @@
+/* $(document).ready(setupHelpers)
+
+function setupHelpers() {
+  $("#html_cd_1").text(
+`<!DOCTYPE html>
+ <html>
+
+      <head>
+
+          <style>
+          </style>
+
+          <script>
+          </script>
+
+      </head>
+
+      <body>
+      </body>
+
+  </html>`
+  )
+
+$("#html_cd_2").text(
+`<p>Hello World!</p>`
+);
+} */
